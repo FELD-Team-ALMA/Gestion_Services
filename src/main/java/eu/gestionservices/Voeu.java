5806a82484aba7a584fea18ce9db34bbd8472c07
Marie-Donnie <delavergnemarie@gmail.com>
2016-11-28 16:49:56 +0100

    Ajout des méthodes de voeu

--- src/main/java/eu/gestionservices/Voeu.java
+++ src/main/java/eu/gestionservices/Voeu.java
@@ -2,4 +2,14 @@ package eu.gestionservices;
 
 public class Voeu {
 
+	private boolean preference;
+	
+	public Voeu(boolean preference){
+		this.preference = preference;
+	}
+	
+	public boolean getPreference(){
+		return this.preference;
+	}
+
 }
